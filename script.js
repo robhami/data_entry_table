3af1f9311bf21f0bf5107d69bb18a804cd11f3e5
robhami <50372518+robhami@users.noreply.github.com>
2020-09-04 20:59:35 +0400

    formatting sorted

--- script.js
+++ script.js
@@ -2,18 +2,26 @@
 let rowCount=1;
 
 function myCreateFunction() {	
+
 	let bhaTable=document.getElementById('BHAentry');
 	let toolRow=document.getElementById('dataRow');
 	let newRow =toolRow.cloneNode(true);
 
 	bhaTable.appendChild(newRow);
 	rowCount++
+	console.log(rowCount);
 	let newNumCol=dataRow[rowCount-1].children[0]
 	newNumCol.textContent=rowCount;
 	console.log(newNumCol);
 }
 
 function myDeleteFunction(row) {	
-	console.log(row.parentElement.parentElement);
-	row.parentElement.parentElement.remove();
+	if(rowCount<2){
+		alert("Can delete final row");
+	} else if (rowCount>1) {
+		console.log(row.parentElement.parentElement);
+		row.parentElement.parentElement.remove();
+		rowCount--;
+		console.log(rowCount);
+	}
 }
